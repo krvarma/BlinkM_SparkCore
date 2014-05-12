c73eeac317850d57ccca84ac53850ac2e0d08c1a
Krishnaraj Varma <krvarma@gmail.com>
2014-05-12 18:46:39 +0530

    Initial Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+BlinkM Smart LED and Spark Core.
+------------------------------------------------
+----------
+BlinkM Smart LED Library for Spark Core
+
+###Wiring###
+ 1. TMP006 Vcc to 3.3V
+ 2. TMP006 GND to GND 
+ 3. TMP006 SCL to D1 (with pullup resistor)
+ 4. TMP006 SDA to D0 (with pullup resistor)
+
+###Sample Application###
+Sample application is simple one to display a color and to blink. The application defines two Spark function setcolor and setblinking to set the color and start/stop blink the LED.
+
+###Web Page###
+The Web Page displays a color picker to select color and a checkbox to strt/stop the blinking. Sepctrum Color picker is used for the Color Picker.
---
+++ blinkm-spark.ino
@@ -0,0 +1,84 @@
+#include "blinkm.h"
+
+const boolean BLINKM_ARDUINO_POWERED = true;
+const int CMD_START_BYTE = 0x01;
+const int serBufLen = 32;
+
+int blinkm_addr = 0x09;
+byte serInBuf[serBufLen];  // array that will hold the serial input string
+int ledPin = 13;
+int r = 255;
+int g = 255;
+int b = 255;
+boolean isBlink = true;
+
+int setrgb(String args){
+    Serial.print("Received: ");
+    Serial.println(args);
+ 
+    char szArgs[13];
+    
+    args.toCharArray(szArgs, 12);
+    
+    sscanf(szArgs, "%d,%d,%d", &r, &g, &b);
+    
+    Serial.println("Color: ");
+    Serial.print(r);
+    Serial.print(",");
+    Serial.print(g);
+    Serial.print(",");
+    Serial.print(b);
+    Serial.println("");
+    
+    if(!isBlink){
+        BlinkM_fadeToRGB(blinkm_addr, r, g, b);
+    }
+    
+    return 0;
+}
+
+int setblink(String args){
+    int blink = args.toInt();
+    
+    
+    isBlink = (1 == blink) ? true : false;
+    
+    if(!isBlink){
+        BlinkM_fadeToRGB(blinkm_addr, r, g, b);
+    }
+    
+    Serial.print("Blinking set to: " + isBlink);
+}
+
+void setup() {
+    Serial.begin(19200); 
+
+    pinMode(ledPin, OUTPUT);
+    
+    if( BLINKM_ARDUINO_POWERED ) {
+        BlinkM_beginWithPower();
+    } 
+    else {
+        BlinkM_begin();
+    }
+    
+    delay(100);  // wait for power to stabilize
+    
+    BlinkM_stopScript(blinkm_addr);
+    BlinkM_setRGB(blinkm_addr, r, g, b);
+
+    Spark.function("setcolor", setrgb);
+    Spark.function("setblinking", setblink);
+}
+
+void loop() {
+    if(isBlink){
+        BlinkM_fadeToRGB(blinkm_addr, r, g, b);
+    
+        delay(1000);
+    
+        BlinkM_fadeToRGB(blinkm_addr, 0, 0, 0);
+    
+        delay(1000);
+    }
+}
